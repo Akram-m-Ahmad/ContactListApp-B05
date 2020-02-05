613968b369cf4c969fdf4e77879df29c3c36bbed
samar3 <samar@codi.tech>
2020-02-05 14:53:44 +0200

    added getContact controller and fetch

--- project enhanced/front/src/App.js
+++ project enhanced/front/src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import Home from './Pages/Home/Home'
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { contacts : [] };
+  }
+ 
+  async componentDidMount(){
+    const response =  await fetch('http://localhost:8080/contacts');
+    const result = await response.json();
+    console.log(result);
+    this.setState({contacts:result, error:"none"});
+  }
   render(){
     return(
       <div className='App'>
-      Hello World!
-      <Home/>
+      Contact List:
+      <ul>
+      {
+        this.state.contacts.map(contact=><li>{contact.name}</li>)
+      }
+     </ul>
       </div>
     )
   }
