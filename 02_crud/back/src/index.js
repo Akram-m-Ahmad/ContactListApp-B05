22d67bb9164544d7e8caf5ff4156a3986101ce1e
samar2 <samarfatayri@gmail.com>
2020-02-13 15:11:09 +0200

    adding crud to front

--- 02_crud/back/src/index.js
+++ 02_crud/back/src/index.js
@@ -1,53 +1,74 @@
 //import {createServer} from 'http';
-import app from './app';
-import initializeDatabase from './db';
+import app from "./app";
+import initializeDatabase from "./db";
 
-const start = async()=>{
-
+const start = async () => {
   const controller = await initializeDatabase();
-  app.get('/', (req, res)=>{
-
-    res.json({message:"Hello"});
-  });
-  
-  app.get('/contacts', async(req, res)=>{
-    const {orderBy} = req.query;
-    const result = await controller.getContacts(orderBy);
-    res.json(result);
+  app.get("/", (req, res, next) => {
+    try {
+      res.json({ message: "Hello" });
+    } catch (err) {
+      next(err);
+    }
   });
 
-  app.get('/contact/:id', async(req, res)=>{
-    const {id} = req.params;
-    const result = await controller.getContactByID(id);
-    res.json(result);
+  app.get("/contacts", async (req, res, next) => {
+    const { orderBy } = req.query;
+    try {
+      const result = await controller.getContacts(orderBy);
+      res.json({ success: true, result });
+    } catch (err) {
+      next(err);
+    }
   });
 
-  app.get('/contacts/create', async(req, res)=>{
-    const{name, email} = req.query;
+  app.get("/contact/:id", async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      const result = await controller.getContactByID(id);
+      res.json({ success: true, result });
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  app.get("/contacts/create", async (req, res, next) => {
+    const { name, email } = req.query;
     console.log(req.query);
-    const result = await controller.createContact({name, email});
-    res.json(result);
+    try {
+      const result = await controller.createContact({ name, email });
+      res.json({ success: true, result });
+    } catch (err) {
+      next(err);
+    }
   });
 
-  app.get('/contacts/delete/:id', async(req, res)=>{
-   
-    const {id}= req.params;
-    const result = await controller.deleteContact(id);
-    res.json(result);
+  app.get("/contacts/delete/:id", async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      const result = await controller.deleteContact(id);
+      res.json({ success: true, result });
+    } catch (err) {
+      next(err);
+    }
   });
 
-  app.get('/contacts/update/:id', async(req, res)=>{
-   
-    const {id}= req.params;
-    const {name, email } = req.query;
-    const result = await controller.updateContact(id, {name, email});
-    res.json(result);
+  app.get("/contacts/update/:id", async (req, res, next) => {
+    const { id } = req.params;
+    const { name, email } = req.query;
+    try {
+      const result = await controller.updateContact(id, { name, email });
+      res.json({ success: true, result });
+    } catch (err) {
+      next(err);
+    }
+  });
+  app.use((err, req, res, next) => {
+    res.status(500).json({ success: false, message: err });
   });
 
-  
-  app.listen(8080, ()=>{console.log("Listening on port 8080")});
-  
-  
-
-}
-start()
+  app.listen(8080, () => {
+    console.log("Listening on port 8080");
+  });
+};
+start();
--- 02_crud/front/src/App.js
+++ 02_crud/front/src/App.js
@@ -1,28 +1,40 @@
-import React, { Component } from 'react';
-import Home from './Pages/Home/Home'
+import React, { Component } from "react";
+import Home from "./Pages/Home/Home";
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = { contacts : [] };
+    this.state = { contacts: [], error: "" };
   }
- 
-  async componentDidMount(){
-    const response =  await fetch('http://localhost:8080/contacts');
-    const result = await response.json();
-    console.log(result);
-    this.setState({contacts:result, error:"none"});
-  }
-  render(){
-    return(
-      <div className='App'>
-      Contact List:
-      <ul>
-      {
-        this.state.contacts.map(contact=><li>{contact.name}</li>)
+
+ getContactList = async()=>{
+    try {
+      const response = await fetch("http://localhost:8080/contacts");
+      const result = await response.json();
+      console.log(result);
+      if (result.success) {
+        this.setState({ contacts: result.result, error: "" });
+      } else {
+        this.setState({ error: result.message });
       }
-     </ul>
+    } catch (err) {
+      this.setState({ error: err });
+    }
+  }
+  async componentDidMount() {
+   this.getContactList();
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.error?<p>{this.state.error}</p>:false}
+        Contact List:
+        <ul>
+          {this.state.contacts.map(contact => (
+            <li>{contact.name}</li>
+          ))}
+        </ul>
       </div>
-    )
+    );
   }
 }
 
